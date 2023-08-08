5a49fada7c4a7ba5a65a560c4f83c5a1f87891aa
Joel <joellongaresjr@gmail.com>
2023-08-08 12:25:28 -0700

    finalized delete product id route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -101,8 +101,21 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No Product found with this id!" });
+    } else {
+      res.status(200).json({ message: "Product deleted successfully" });
+    }
+  }
 });
 
 module.exports = router;
